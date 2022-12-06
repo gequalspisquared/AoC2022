6a2654d6b866c506875063d1b2a58ad1b4c1db47
Nicholas Crane <nickcrusher30@gmail.com>
2022-12-05 22:02:58 -0500

    Solve Day 5 problems A and B

---
+++ src/d5a.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+
+void move(size_t num, std::vector<char> &fromStack, std::vector<char> &toStack)
+{
+    for (size_t i = 0; i < num; i++) {
+        toStack.push_back(fromStack.back());
+        fromStack.pop_back();
+    }
+}
+
+int main()
+{
+    const size_t numStacks = 9;
+    std::ifstream cratesFile("inputs/d5.txt");
+    std::string cratesLine;
+    std::vector<char> crates[numStacks];
+
+    while (getline(cratesFile, cratesLine)) {
+        if (cratesLine.substr(1, 1) == "1") break;
+        for (size_t i = 0; i < numStacks; i++) {
+            if (cratesLine.substr(4*i + 1, 1) == " ") continue;
+            crates[i].push_back(cratesLine[4*i + 1]);
+        }
+    }
+
+    for (size_t i = 0; i < numStacks; i++)
+        std::reverse(crates[i].begin(), crates[i].end());
+    
+    size_t num, from, to;
+    std::string blank;
+    while (cratesFile >> blank >> num >> blank >> from >> blank >> to) {
+        move(num, crates[from - 1], crates[to - 1]);
+    }
+
+    for (size_t i = 0; i < numStacks; i++)
+        std::cout << crates[i].back();
+    std::cout << "\n";
+}
